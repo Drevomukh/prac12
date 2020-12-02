bcbd3a68fccf551b9a871df1ef38b8fae5c99925
Drevomukh <70793125+Drevomukh@users.noreply.github.com>
2020-12-02 11:47:18 +0300

    first commit

---
+++ src/Ex4/Main.java
@@ -0,0 +1,8 @@
+package Ex4;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new TelephoneParser("89174356321"));
+        System.out.println(new TelephoneParser("+79274356522"));
+    }
+}
---
+++ src/ex1/Main.java
@@ -0,0 +1,8 @@
+package ex1;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("Ivan", "Ivanov", "Ivanovich");
+        System.out.println(p.getInfo());
+    }
+}
